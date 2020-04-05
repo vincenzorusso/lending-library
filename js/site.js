c46fa35ae84e5f3ce3534f54ec90d14ab216481e
vincenzorusso <vi.russo@gmail.com>
2020-04-05 18:05:33 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -55,9 +55,9 @@ function toggleAvailable(state) {
 
     // Update the button text
     if (button.hasClass('button-pressed')) {
-        button.html("Show All")
+        button.html("Mostra tutti")
     } else {
-        button.html("Show Available")
+        button.html("Mostra disponibili")
     }
 }
 
